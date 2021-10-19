7cc6eaa5d3edcb3ce4eb40950aa60048d35146cc
Alexandre Morais <alexandre@a2o.com.br>
2021-10-19 13:48:13 -0300

    Feito a authenticação e geração do token

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -5,8 +5,12 @@ class AuthenticateUserController {
   async handle(request: Request, response: Response) {
     const { code } = request.body;
     const service = new AuthenticateUserSercice();
-    const result = await service.execute(code);
-    return response.json(result);
+    try {
+      const result = await service.execute(code);
+      return response.json(result);
+    } catch (error) {
+      return response.json({ error: error.message })
+    }
   }
 }
 
