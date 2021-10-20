e99a529fb59f4b0b94601cd27842ae91397e5949
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2021-10-19 23:37:04 -0300

    Criado o serviço que pega as 3 ultimas mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
+
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMassegeController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle)
-router.post("/messages", ensureAuthenticated, new CreateMassegeController().handle)
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated, new CreateMassegeController().handle);
+
+router.get("/messages/Last3", new GetLast3MessagesController().handle);
 
 export { router }
